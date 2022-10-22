d4d05ce21dd6a6b982db6e120329700b47e2a595
Jose maria Lebre <jlebre@c2r5s4.42lisboa.com>
2022-10-22 23:53:10 +0100

    Final Project

--- basic_tests.sh
+++ basic_tests.sh
@@ -3,49 +3,70 @@
 make re
 sleep 1
 clear
-echo "STARTING TESTS\n"
+echo "\033[0;32mSTARTING TESTS\n"
 sleep 1
-echo "\033[0m\nTest 1:"
+echo "\nTest 1:"
 echo "1 Philosopher"
 echo "Time to die 800ms"
 echo "Time to eat 200ms"
-echo "Time to sleep 200ms\n\033[0;32m"
+echo "Time to sleep 200ms\n"
+echo "\nPhilosopher should not eat, and should die!\n\033[0m"
+sleep 3
 ./philo 1 800 200 200
 sleep 1
-echo "\033[0m\nTest 2:"
+echo "\033[0;32m\nTest 2:"
 echo "5 Philosophers"
 echo "Time to die 800ms"
 echo "Time to eat 200ms"
-echo "Time to sleep 200ms\n\033[0;32m"
-while ./philo 5 800 200 200; do sleep 10; done
+echo "Time to sleep 200ms\n"
+echo "\nNo Philosopher should die! Testing for 15 sec!\n\033[0m"
+sleep 3
+./philo 5 800 200 200 &
+PID=$!
+sleep 15
+kill $PID
 sleep 1
-echo "\033[0m\nTest 3:"
+echo "\033[0;32m\nTest 3:"
 echo "5 Philosophers"
 echo "Time to die 800ms"
 echo "Time to eat 200ms"
 echo "Time to sleep 200ms"
-echo "Number of meals 7\n\033[0;32m"
+echo "Number of meals 7\n"
+echo "\nAll Philosopher should eat at least 7 meals!\n\033[0m"
+sleep 3
 ./philo 5 800 200 200 7
 sleep 1
-echo "\033[0m\nTest 4:"
+echo "\033[0;32m\nTest 4:"
 echo "4 Philosophers"
 echo "Time to die 410ms"
 echo "Time to eat 200ms"
-echo "Time to sleep 200ms\n\033[0;32m"
-while ./philo 4 410 200 200; do sleep 10; done
+echo "Time to sleep 200ms\n"
+echo "\nNo Philosopher should die! Testing for 15 sec!\n\033[0m"
+sleep 3
+./philo 4 410 200 200 &
+PID=$!
+sleep 15
+kill $PID
 sleep 1
-echo "\033[0m\nTest 5:"
+echo "\033[0;32m\nTest 5:"
 echo "4 Philosophers"
 echo "Time to die 310ms"
 echo "Time to eat 200ms"
-echo "Time to sleep 100ms\n\033[0;32m"
+echo "Time to sleep 100ms\n"
+echo "\nOne Philosopher should die!\n\033[0m"
+sleep 3
 ./philo 4 310 200 100
 sleep 1
-echo "\033[0m\nTest 6:"
+echo "\033[0;32m\nTest 6:"
 echo "2 Philosophers"
 echo "Time to die 800ms"
 echo "Time to eat 200ms"
-echo "Time to sleep 200ms\n\033[0;32m"
-while ./philo 2 800 200 200; do sleep 10; done
+echo "Time to sleep 200ms\n\033[0m"
+echo "\nNo Philosopher should die! Testing for 15 sec\n\033[0m"
+sleep 3
+./philo 2 800 200 200 &
+PID=$!
+sleep 15
+kill $PID
 sleep  1
-echo "\033[0m\n\nFinished Successfully!\n"
+echo "\033[0;32m\n\nFinished Successfully!\n\033[0m"
